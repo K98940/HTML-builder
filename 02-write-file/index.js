5e2894155f2d7a81517e34b68749387416615779
k98940 <pv1xxb@yandex.ru>
2024-01-15 22:04:09 +0600

    feat: finish the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const { createWriteStream } = require('fs');
+const { join } = require('path');
+const { stdin, stdout } = require('process');
+const { EOL } = require('os');
+const readline = require('readline');
+
+const colors = {
+  bold: '\x1b[1m',
+  green: '\x1b[32m',
+  red: '\x1b[31m',
+  reset: '\x1b[0m',
+};
+const fileName = 'writeStream.txt';
+const filePath = join(__dirname, fileName);
+
+const writeStream = createWriteStream(filePath, { encoding: 'utf-8' });
+const rl = readline.createInterface(stdin, stdout);
+rl.setPrompt(colors.green + 'you can type text here:' + EOL + colors.reset);
+rl.prompt();
+
+rl.on('line', (line) => {
+  if (line.match(/(exit)/i)) {
+    rl.close();
+    return;
+  }
+  writeStream.write(line + EOL);
+}).on('close', () => {
+  console.log(colors.green, EOL + 'this is the end', colors.reset);
+});
+
+writeStream.on('error', (err) => {
+  console.log(colors.red, 'Oh, i have error: ' + err, colors.reset);
+});
